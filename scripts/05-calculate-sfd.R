28142b1fa423b7191e42c4ebf0f0574d5afe23fd
Jessica Guo <jessicaguo@email.arizona.edu>
2023-04-05 13:21:34 -0700

    Join with identifying variables

--- scripts/05-calculate-sfd.R
+++ scripts/05-calculate-sfd.R
@@ -2,9 +2,10 @@
 # Calculate sap flux
 library(dplyr)
 library(TREXr)
-library(ggplot2)
+library(lubridate)
+library(readr)
 
-l# Load data to baseline
+# Load data to baseline
 baseline <- readRDS("app/baseline.RDS")
 range(lapply(baseline, length))
 # 92 sensors with between 2 and 58 consecutive time chunks
@@ -31,15 +32,28 @@ for(i in 1:length(baseline)) { # number of sensors
 
   # Extract relevant outputs to dataframe (including probe_id and parameters)
   sfd_list[[i]] <- cbind.data.frame(probe_id = names(baseline)[i],
-                                    timestamp = as.POSIXct(baselined$input$timestamp, tz = "GMT"),
+                                    # Convert timestamp back to America/Los_Angeles
+                                    timestamp = with_tz(as.POSIXct(baselined$input$timestamp, tz = "GMT"), tzone = "America/Los_Angeles"),
                                     deltaV = baselined$input$value,
                                     max.mw = baselined$max.mw$value,
                                     k = sfd$sfd.mw$k,
                                     sfd = sfd$sfd.mw$sfd,
                                     q025 = sfd$sfd.mw$q025,
                                     q975 = sfd$sfd.mw$q975)
+  
+  print(paste0("Sensor ", i, " of 92 completed"))
 }
 
 # Bind all sfd for all 92 sensors
 sfd_all <- do.call(rbind, sfd_list)
+
+# Load and join with identifying tables
+probes <- read_csv("data_raw/sapflow_csv/probe.csv") # to join_by(probe_id), links to veg_id
+veg <- read_csv("data_raw/sapflow_csv/veg.csv")
+
+sfd_combined <- sfd_all %>%
+  left_join(select(probes, probe_id, veg_id), by = "probe_id") %>%
+  left_join(select(veg, veg_id, veg_type, site_name, diameter_cm), by = "veg_id")
+
+saveRDS(sfd_combined, file = "data_clean/SFD_all.RDS")
 
